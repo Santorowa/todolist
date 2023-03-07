f83b56ada2e5b6db3e58f95fb9361daac338436d
Hoi In Ha <ineee199105@gmail.com>
2023-03-08 00:23:37 +0900

    Lenox

--- IdeaProjects/todolist/src/main/java/Controller/HomeController.java
+++ IdeaProjects/todolist/src/main/java/Controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping(value = "/")
     public String main() {
-        System.out.println("Lenox");
+        System.out.println("Hello world!");
 
         return "index";
     }
